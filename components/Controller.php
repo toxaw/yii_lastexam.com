e40434f392625444cb1599acb63c7a059447229f
Anton <antonser2@gmail.com>
2019-04-16 02:21:49 +0400

    агроницение по доступности урлов в зависимсти от роли

--- components/Controller.php
+++ components/Controller.php
@@ -1,14 +1,60 @@
 <?php
 namespace app\components;
 
+use yii\web\NotFoundHttpException;
+
 use Yii;
 
 class Controller extends \yii\web\Controller
 {
 	public function beforeAction($action)
 	{
-		
+		$admin = [];
 
-		return true;
+		$user = [];
+
+		$guest = [];
+
+		$admin['site'] = ['index', 'logout', 'error'];
+
+		$admin['claim'] = ['allclaim','edit'];
+
+		$admin['category'] = [];
+
+		$user['site'] = ['index', 'logout', 'error'];
+
+		$user['claim'] = ['myclaim','add'];
+
+		$guest['site'] = ['index', 'login','registr', 'error'];
+
+		$c = Yii::$app->controller->id;
+
+		$a = Yii::$app->controller->action->id;
+
+		if(!Yii::$app->user->isGuest && Yii::$app->user->identity->is_admin) 
+        {
+        	if(isset($admin[$c]) && ( !$admin[$c] || in_array($a, $admin[$c])))
+        	{
+        		return true;
+        	}
+        }
+        else if (!Yii::$app->user->isGuest)
+        {
+        	if(isset($user[$c]) && ( !$user[$c] || in_array($a, $user[$c])))
+        	{
+        		return true;
+        	}
+        }
+        else
+        {
+        	if(isset($guest[$c]) && ( !$guest[$c] || in_array($a, $guest[$c])))
+        	{
+        		return true;
+        	}
+        }
+
+		Yii::$app->getResponse()->redirect('basic/site/error')->send();
+
+		return false;
 	}
 }
